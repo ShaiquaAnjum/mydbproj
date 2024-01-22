f3d3239ae629701acd731e7ab9720d454bec292d
ShaiquaAnjum <shaiquak76@gmail.com>
2024-01-22 17:08:23 +0530

    127: create save post feauture

---
+++ src/main/java/com/demo/mydbproj/MydbprojApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mydbproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydbprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydbprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mydbproj/MydbprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mydbproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydbprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
